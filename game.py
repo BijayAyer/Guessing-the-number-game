c992f49d749653284f22678dfac3055d81eaafdc
BijayAyer <bijayayer68@gmail.com>
2025-09-08 11:46:13 +0545

    flappy bird game

--- game.py
+++ game.py
@@ -1,22 +1,21 @@
 import random
 
-count = 0
+count=0
 
 print("Welcome to the Number Guessing Game.\n")
-
-a = int(input("enter the start point of the range of guess"))
-b = int(input("enter the end point of guessing the number "))
-right_number = random.randint(a, b)
+a=int(input("enter the start point of the range of guess"))
+b=int(input("enter the end point of guessing the number "))
+right_number = random.randint(a,b)
 
 print("I've picked a number for you to guess.")
-print("The number is between", a, "and ", b, "\n")
+print("The number is between",a,"and ",b,"\n")
 
 guessed_number = int(input("Guess a number:"))
 
 
 while guessed_number != right_number:
-    count = count+1
-    if guessed_number < 1 or guessed_number > 5:
+    count=count+1
+    if guessed_number<1 or guessed_number>5:
         print("Invalid guess. Please enter a number between 1 and 5.")
 
     elif guessed_number > right_number:
@@ -29,8 +28,8 @@ while guessed_number != right_number:
         print("Choose a higher number.\n")
 
     guessed_number = int(input("Enter guess number again:\n "))
-
+    
 
 print("\nCorrect guess.")
 print("You won. Thank you for playing.")
-print("you count score is ", count)
+print("you count score is ",count)
